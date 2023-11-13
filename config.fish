96d86f53b63af0539b41e8ef09b8253bf4ae96cb
Aleksandr Shtaub <91491081+aleksandr-shtaub@users.noreply.github.com>
2023-11-13 11:08:28 +0200

    Init config

---
+++ config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+#set -x XDG_CONFIG_HOME "~/.config"
+set -x HOMEBREW_BUNDLE_FILE_GLOBAL "~/.config/brew/Brewfile"
+set -x EDITOR nvim
+set -x GPG_TTY (tty)
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+
+abbr --add gpg-keys gpg --list-secret-keys --keyid-format=long
+
---
+++ plugins/iterm2_intergration/functions/install_iterm2_shell_integration.fish
@@ -0,0 +1,4 @@
+function install_iterm2_integration
+    curl -fsSL https://iterm2.com/shell_integration/fish -o $__fish_config_dir/conf.d/xx_iterm2_shell_integration.fish
+end
+
