c58296009bf9d156682b2dd0e80fb07a4c801b90
Aleksandr Shtaub <91491081+aleksandr-shtaub@users.noreply.github.com>
2023-11-22 23:27:33 +0200

    Update configs

--- config.fish
+++ config.fish
@@ -8,8 +8,12 @@ set -x HOMEBREW_BUNDLE_FILE_GLOBAL "~/.config/brew/Brewfile"
 set -x EDITOR nvim
 set -x GPG_TTY (tty)
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
+set -x PYENV_ROOT $HOME/.pyenv
+set fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
 abbr --add gpg-keys gpg --list-secret-keys --keyid-format=long
 
+
 pyenv init - | source
+direnv hook fish | source
+
